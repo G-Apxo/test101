7c1485d4bf3210b3a49b05130eb179d136808bed
G-Apxo <39370807+GiorgiApkhadze@users.noreply.github.com>
2022-05-29 01:56:13 +0400

    oop update

--- js2.js
+++ js2.js
@@ -80,17 +80,139 @@ var people =["Tom", "Alice","Bob", "Sam"];
 // }
 // document.write(y+ "</br>" +z);
 
-function display (x,testmessage) {
-   var message = testmessage(x);
-    document.write(message);
-}
-function welcomeMesasge(time){
-    if (time<12)
-    return "Good Morning";
-    else if (time<18)
-    return "Good Afternoon";
-    else
-    return "Good Evening";
-}
-display (12,message);
+// function display (x,testmessage) {
+//    var message = testmessage(x);
+//     document.write(message);
+// }
+// function welcomeMesasge(time){
+//     if (time<12)
+//     return "Good Morning";
+//     else if (time<18)
+//     return "Good Afternoon";
+//     else
+//     return "Good Evening";
+// }
+// display (12,message);
 
+
+
+// function getFactorial (n) {
+//     if (n===1){
+//         return 1;
+//     }
+//     else{
+//         return n*getFactorial(n-1);
+//     }
+// }
+// var result = getFactorial(4);
+// document.write(result);
+//  result = 4 * getFactorial(3);
+//  result = 4 * 3 * getFactorial(2);
+//  result = 4 * 3 * 2 * getFactorial(1);
+//24
+
+// hoisting
+display();
+function display(){
+    console.log("hello hoisting")
+}
+
+var c = a*b; //undefined
+var a = 7;
+var b = 3;
+
+console.log(c); //nan
+
+
+console.log(foo) //undefiend
+var foo = "hello";
+
+// OOP (object oriented programming)
+
+
+
+// var user = new Object();
+
+// var user = {};
+//     user.name = "Tom",
+//     user.age = 26
+// console.log(user.name);
+
+
+
+// user.name  = "Tom";
+// user.age = 26;
+
+//     user.display = function(){
+//         console.log(this.name);
+//     }
+
+//     user.display();
+
+// var user = {
+//     name: "Tom",
+//     age: 26,
+//     display: function(){
+//         console.log(this.age);
+//     }
+// };
+
+// user.display();
+
+
+// var user = {
+//     name: "Tom",
+//     age :18,
+//     display : function(){
+//     }
+// };
+
+// console.log(user.name);
+// delete user.name;
+// console.log(user.name);
+
+
+// var country = {
+//     name: "USA",
+//     language : "english",
+//       capital:{
+//             name: "Washington",
+//             population:12312312,
+//             year:1234
+//       }   
+// };
+
+// console.log("დედაქალაქი" + country.capital.name);
+// console.log("მოსახლეობა" + country.capital.population);
+// console.log("დაარსების წელი" + country.capital.year)
+
+// var cities = [
+//     {name:"ცურიხი", population:31232131},
+//     {name:"ჟენევა", population:12313},
+//     {name:"რა,ე", population:13123211231}
+// ]
+
+var country = {
+        name:"შვეიცარია",
+        languages: ["გერმანული" , "ფრანგული" , "იტალიური"],
+        capital:{
+            name:"ბერნი",
+            population:123123
+        },
+        cities :[
+            {name:"ცურიხი", population:31232131},
+            {name:"ჟენევა", population:12313},
+            {name:"რამე", population:13123211231}
+        ]
+}
+
+// ყველა ენის გამოტანა
+    document.write("შვეიცარიის ოფიციალური ენებია");
+    for (var i=0; i<country.languages.length; i++) {
+     document.write(country.languages[i]);
+    }
+
+    document.write("ყველა ელემენტი ქლაქებიდან")
+    for (var i=0; i<country.cities.length; i++){
+        document.write(country.cities[i].name + country.cities[i].population)
+    }e
