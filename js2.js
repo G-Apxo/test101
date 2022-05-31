78137f9bf6a9b9ffbeeb5a4e0aecfb572285dc9a
G-Apxo <39370807+GiorgiApkhadze@users.noreply.github.com>
2022-06-01 00:17:25 +0400

    Update js2.js

--- js2.js
+++ js2.js
@@ -207,12 +207,268 @@ var country = {
 }
 
 // ყველა ენის გამოტანა
-    document.write("შვეიცარიის ოფიციალური ენებია");
-    for (var i=0; i<country.languages.length; i++) {
-     document.write(country.languages[i]);
+    // document.write("შვეიცარიის ოფიციალური ენებია");
+    // for (var i=0; i<country.languages.length; i++) {
+    //  document.write(country.languages[i]);
+    // }
+
+    // document.write("ყველა ელემენტი ქლაქებიდან")
+    // for (var i=0; i<country.cities.length; i++){
+    //     document.write(country.cities[i].name + country.cities[i].population)
+    // }
+
+
+
+    // function createUser( pName, pAge ){
+    //     return {
+    //         name:pName,
+    //         age:pAge,
+    //         displayInfo:function(){
+    //             document.write("სახელი" + this.name + "ასაკი" +this.age +"</br>");
+    //         }
+    //     };
+    // };
+    //     var Tom = createUser("Tom", 26);
+    //     Tom.displayInfo();
+
+    //     var giorgi = createUser("Giorgi" ,24);
+    //     giorgi.displayInfo();
+
+
+    // var tom = new Object();
+
+
+    // function User (pName, pAge){
+    //     this.name = pName;
+    //     this.pAge = pAge;
+
+    //     this.displayInfo = function(){
+    //         document.write ("saxeli" + this.name + "asaki" + this.age)
+            
+    //     }
+    // }
+
+    //     var tom = new User("Tom", 26);
+    //     console.log(tom.name);
+    //     tom.displayInfo();
+
+
+    // function createUser( pName, pAge){
+    //     return {
+    //         name:pName,
+    //         age:pAge,
+    //         displayInfo:function(){
+    //             document.write("სახელი" + this.name + "ასაკი" +this.age +"</br>");
+    //         },
+    //         dirveCar:function(car){
+    //             document.write(this.name + "მანქანის მარკა" + car.name + "წელი" +car.year + "</br>");
+    //         }
+    //     }
+    // };
+
+    // function createCar(mName, mYear){
+    //     return{
+    //         name:mName,
+    //         year:mYear
+    //     }
+    // }
+    // var Tom = createUser("Tom", 26);
+    // Tom.displayInfo()
+
+    // var tesla = createCar("Tesla" ,2008);
+
+    // Tom.dirveCar(tesla);
+
+
+        /// car constructor
+    function Car(mName , mYear) {
+        this.name = mName;
+        this.year = mYear;
+        this.getCarInfo = function(){
+            document.write("modeli" + this.name+  "gamsovhebis weli" + this.year);
+        };
+    };
+
+    // user constructor
+
+    function User (pName, pAge){
+        this.name = pName;
+        this.age = pAge;
+        this.driveCar = function(car) {
+            document.write ( this.name +  "yavs manqana" + car.name )
+        };
+        this.displayInfo = function() {
+            document.write("saxeli" + this.name + " asaki "+ this.age)
+        }
+    }
+    var tom = new User("Tom", 26);
+    tom.displayInfo();
+
+    var tesla = new Car("Tesla" ,2008);
+
+    tom.driveCar(tesla);
+
+/////////////////////////////////   prototype      ///////////////////////////////////////////////////
+    function User (pName , pAge) {
+        this.name = pName;
+        this.age = pAge;
+        this.displayInfo = function(){
+            document.write("saxeli" + this.name + "asaki" +this.age);
+        }
     }
 
-    document.write("ყველა ელემენტი ქლაქებიდან")
-    for (var i=0; i<country.cities.length; i++){
-        document.write(country.cities[i].name + country.cities[i].population)
-    }e
+    User.prototype.hello = function(){
+        document.write (this.name + "says hello")
+    }
+
+    User.prototype.maxAge = 110;
+
+    var tom = new User ("tom" , 20);
+    tom.hello();
+
+    var jhon = new User ("jhon", 100);
+    jhon.hello();
+
+
+    /////////////////////////////////   ინკაფსულაცია      ///////////////////////////////////////////////////
+
+ 
+    // (getter)
+    // (setter)
+
+
+    function User (name, age) {
+        this.name = name ;
+        var _age = age;
+        this.displayInfo = function (){
+            document.write ("saxeli" + this.name + "asaki" + _age+ "</br>");
+        };
+        this.getAge = function() {
+            return _age;
+        }
+        this.setAge = function(age) {
+            if (typeof age === "number" && age >0 && age <100) {
+                return _age=age;
+            }
+            else{
+                console.log("araswori mnishveloba")
+            }
+        }
+    }
+
+
+    // var tom = new User ("tom" , 26);
+
+    // console.log (tom._age); //26   undefined   
+    // console.log(tom.getAge()); // 26
+
+    // tom.setAge(32);
+
+    // console.log(tom.getAge()); // 32 
+    // tom.setAge("54"); // araswori mnishvneloba
+
+
+    // console.log( typeof 42 ); // number
+ 
+    // console.log( typeof "42" );// string 
+
+    // console.log( typeof "true");// boolean
+
+    // console.log( instanceof)
+
+
+    // function Car(make,mode,year) {
+    //     this.make = make;
+    //     this.mode = mode;
+    //     this.year = year;
+    // }
+    // const auto = new Car ('Honda' , "crv" , 1998);
+
+    // console.log (auto instanceof Car);
+
+    // console.log (auto instanceof Object);
+
+
+
+        // functon
+        // 1) arguments პარამეტრების მასივი
+        // 2)lengt პარამეტრების რაოდენობა რასაც ფუნქცია ელოდება
+        // 3)caller გნასზაღვრავს ფუნქციას რომელიც წამოიწყო ფუნქციის მიმდინარე შესრულება
+        // 4)name ფუნქციის სახელი
+        // 5)protoype ფუნქციის პროტიტპი
+
+    // call apply  
+
+    // function add(x,y){
+    //     return x+y;
+    // }
+
+    // var result = add.call(this , 3,8);
+
+    // console.log(result) // 11
+
+
+
+    // function add(x,y){
+    //     return x+y;
+    // }
+
+    // var result = add.apply(null , [3,8]);
+
+    // console.log(result) //11
+
+
+
+    // function User (name,age) {
+    //     this.name = name;
+    //     this.age = age;
+    // }
+    // var tom = new User ("tom" , 24) ;
+
+    // function display() {
+    //     console.log("misi saelia" + this.name);
+    // }
+
+    // display.call(tom);
+
+        /////////////////////////////////   inheritance      ///////////////////////////////////////////////////
+
+        function User (name, age) {
+            this.name = name;
+            this.age = age;
+            this.go = function() {
+                document.write(this.name + "he is going")
+            };
+            this.displayInfo = function(){
+                document.write ("saxeli" + this.name + "asaki" + this.age)
+            };
+        }
+        User.prototype.maxAge = 100;
+
+        function Employee(name,age,comp) {
+            User.call(this ,name ,age) ;
+            this.company = comp;
+            this.displayInfo = function(){
+                document.write ("saxeli" + this.name + "asaki" + this.age + "company" + this.company )
+            };
+        }
+
+        Employee.prototype = Object.create(User.prototype);
+
+        var tom = new User ("tom", 26);
+        var bill = new Employee ("bill",32,"Tesla")
+
+        tom.go();
+        bill.go();
+
+        tom.displayInfo();
+        bill.displayInfo();
+
+        /////////////////////////////////   This      ///////////////////////////////////////////////////
+
+        this.alert("global alert")
+
+        function foo () {
+            var bar = "test"
+            console.log (bar);
+        }
